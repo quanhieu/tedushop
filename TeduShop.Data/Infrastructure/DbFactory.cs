d93202f0b83a92ea2c9f9d033e09e6993e3092c2
SOI-LAN-TRON <truongquanghieu8198@gmail.com>
2018-05-13 19:00:32 +0700

    add entity framework code first for data

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace TeduShop.Data.Infrastructure
+{
+    // DbFactory de khoi tao doi tuong DbContext thay vi build truoc tiep thi se thongg qua doi tuong Factory nay
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TeduShopDbContext dbContext;
+
+        public TeduShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TeduShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}

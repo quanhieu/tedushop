00fae5be62d5ea39db44fa8937ff15d5d2b32d25
SOI-LAN-TRON <truongquanghieu8198@gmail.com>
2018-05-14 00:01:52 +0700

    commit migration and reponsitory

--- TeduShop.Model/Models/Product.cs
+++ TeduShop.Model/Models/Product.cs
@@ -16,20 +16,30 @@ namespace TeduShop.Model.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { set; get; }
+
         [Required]
         [MaxLength(256)]
         public string Name { set; get; }
+
         [Required]
         [MaxLength(256)]
         public string Alias { set; get; }
+
+        [Required]
         public int CategoryID { set; get; }
+
         [MaxLength(256)]
         public string Image { set; get; }
-        public XElement MoreImages { set; get; }
-        [Required]
+
+        [Column(TypeName = "xml")]
+        public string MoreImages { set; get; }
+
         public decimal Price { set; get; }
+
         public decimal? PromotionPrice { set; get; }
+
         public int? Warranty { set; get; }
+
         [MaxLength(500)]
         public string Description { set; get; }
         public string Content { set; get; }
@@ -38,7 +48,15 @@ namespace TeduShop.Model.Models
         public bool? HotFlag { set; get; }
         public int? ViewCount { set; get; }
 
+        public string Tags { set; get; }
+
+        public int Quantity { set; get; }
+
+        public decimal OriginalPrice { set; get; }
+
         [ForeignKey("CategoryID")]
         public virtual ProductCategory ProductCategory { set; get; }
+
+        public virtual IEnumerable<ProductTag> ProductTags { set; get; }
     }
 }
